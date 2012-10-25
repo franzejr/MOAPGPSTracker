a2a0dd9c22d3822141d59bd59770b8b2a3c7da57
Franzé Jr <franzejr@gmail.com>
2012-10-25 20:40:21 -0300

    [#] first commit

---
+++ src/moap/gpstracker/MOAPGpsTracker.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moap.gpstracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author igobrilhante
+ */
+public class MOAPGpsTracker extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        
+    }
+}
